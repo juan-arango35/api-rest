f980312510df3d98996d2b74d1050fa7e4a3a0db
juan arango <juanarangozapata35@gmail.com>
2024-12-17 08:39:39 -0500

    agrega crear una categoria

--- src/routes/categories.routes.js
+++ src/routes/categories.routes.js
@@ -1,17 +1,38 @@
 import { Router } from "express";
 import { prisma } from "../db.js";
-const router = Router()
+const router = Router();
 
+//traer categorias
+router.get("/categories", async (req, res) => {
+  const categories = await prisma.category.findMany({
+    include: {
+      products: true,
+    },
+  });
+  res.json(categories);
+});
 
-router.get("/categories", async(req, res) => {
-   const categories = await prisma.category.findMany({
-      include:{
-         products: true
+//crear categorias
+router.post("/categories", async (req, res) => {
+  try {
+    const { name } = req.body;
+    if(!name) return res.status(400).json({error:"El nombre es obligatorio"})
+    const newcategory= await prisma.category.create({
+      data:{
+        name:name
       }
-   })
-   res.json(categories)
-})
+    })
+    res.status(201).json(newcategory)
+  } catch (error) {
+   console.error("Error al crear el producto:", error);
+   console.error(error)
+res.status(500).json({ error: "Hubo un error al crear el producto." });
+  }
+});
+
+//para eliminar un producto:
 
 
 
-export default router
+
+export default router;
